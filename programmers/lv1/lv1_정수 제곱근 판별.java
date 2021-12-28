5d81d89f5d0b4f5254c34c855c37c1d54f1ac098
MiJin9 <kkk_3339@naver.com>
2021-12-28 18:29:00 +0900

    21.12.28(화)

---
+++ programmers/lv1/lv1_정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        
+        for(long i=1; i<=n; i++){
+            if(n == i * i){
+                answer = (i + 1) * (i + 1);
+                break;
+            }
+        }
+        return answer;
+    }
+}
