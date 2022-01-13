3328ba29b08520fee97df218ed5972c6a0910937
MiJin9 <kkk_3339@naver.com>
2022-01-13 12:11:27 +0900

    21.01.13(목)

---
+++ programmers/lv2/lv2_숫자의 표현.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sum = 0;
+        
+        for(int i=1; i<=n; i++){
+            sum = 0;
+            for(int j=i; j<=n; j++){
+                sum += j;
+                if(sum>n)
+                    break;
+                else if(sum==n){
+                    answer++;
+                    break;
+                }
+            }   
+        }
+        return answer;
+    }
+}
