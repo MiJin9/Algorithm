54cb870b2a358a0063d2838e70792a0a4005609b
MiJin9 <kkk_3339@naver.com>
2022-01-14 12:31:57 +0900

    21.01.14(금)

---
+++ programmers/lv2/lv2_다음 큰 숫자.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int changeNum(int n) {
+		String num2 = "";
+		int count = 0;
+		
+		num2 = Integer.toBinaryString(n);
+		
+		for(int i=0; i<num2.length(); i++) {
+			if(num2.charAt(i)=='1') {
+				count++;
+			}
+		}
+		return count;
+	}
+    
+    public int solution(int n) {
+        int n2 = n+1;
+        
+        while(true) {
+			if(changeNum(n)==changeNum(n2))
+				break;
+			else
+				n2++;
+		}
+        
+        return n2;
+    }
+}
