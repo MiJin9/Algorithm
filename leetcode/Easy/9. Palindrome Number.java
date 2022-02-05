7209eb69ab5fdc3bc39b7c9968a844a0f50ea266
MiJin9 <kkk_3339@naver.com>
2022-02-05 13:23:26 +0900

    22.02.05(토)

---
+++ leetcode/Easy/9. Palindrome Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = x+"";
+        
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i)!=str.charAt(str.length()-1-i))
+                return false;
+        }
+        return true;
+    }
+}
