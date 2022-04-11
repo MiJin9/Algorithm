48b9e903b1f09f5a6efbe1a915a927d2b229215b
MiJin9 <kkk_3339@naver.com>
2022-04-11 21:38:58 +0900

    22.04.11(월)

---
+++ leetcode/Easy/203. Remove Linked List Elements.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) head = head.next;
+        ListNode curr = head;
+        while (curr != null && curr.next != null)
+            if (curr.next.val == val) curr.next = curr.next.next;
+            else curr = curr.next;
+        return head;
+    }
+}
