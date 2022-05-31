0e529b88ef534c6ca5fbdb10b1a299e60cf61896
MiJin9 <kkk_3339@naver.com>
2022-05-31 23:20:29 +0900

    22.05.31(화)

--- leetcode/Medium/48. Rotate Image.java
+++ leetcode/Medium/48. Rotate Image.java
@@ -1,14 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < (n + 1) / 2; i ++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = temp;
-            }
-        }
+        for (int i = 0; i < matrix.length; i++) {
+		for (int j = i+1; j < matrix[i].length; j++) {
+			int tmp = matrix[j][i];
+			matrix[j][i] = matrix[i][j];
+			matrix[i][j] = tmp;
+
+		}
+	}
+		
+	for (int i = 0; i < matrix.length; i++) {
+		for (int j = 0; j < matrix[i].length/2; j++) {
+			int tmp = matrix[i][j];
+			matrix[i][j] = matrix[i][matrix[i].length-1-j];
+			matrix[i][matrix[i].length-1-j] = tmp;
+				
+		}
+	}	
     }
 }
