1bbacf0e2a98a8dd69ba60660bcc969a61c08ea2
MiJin9 <kkk_3339@naver.com>
2022-07-01 21:07:58 +0900

    22.07.01(금)

---
+++ baekjoon/15650. N과 M (2).java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Main{
+    static int n,m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n+1];
+        
+        func(1);
+        System.out.println(sb);
+    }
+    static void func(int k){
+        if(k==m+1){
+            for(int i=1; i<=m; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+        int start = arr[k-1];
+        if(start==0)
+            start = 1;
+        
+        for(int i=start; i<=n; i++){
+            boolean check = false;
+            for(int j=0; j<k; j++){
+                if(i==arr[j])
+                    check = true;
+            }
+            if(!check){
+                arr[k] = i;
+                func(k+1); 
+            }
+            
+        }
+    }
+}
